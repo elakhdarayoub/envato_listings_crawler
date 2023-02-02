eb0ee886efcb2bf5ab06ed906ee4e888479d466a
elakhdar ayoub <raja500.ayoub@gmail.com>
2023-02-02 12:57:50 +0100

    a working version

--- crawler.py
+++ crawler.py
@@ -12,12 +12,8 @@ import requests
 #Starts the driver and goes to our starting webpage
 ## use the webdriver version that corresponds to your browsers version
 ### if you want to use a browser other than chrome change .Chrome() with the name of the browser you want
-driver = webdriver.Chrome('') #put the driver path between ()
+driver = webdriver.Chrome('C:/Users/user/chromedriver.exe') #put the driver path between ()
 driver.get('https://elements.envato.com/graphic-templates/print-templates+ux-and-ui-kits+websites+logos/sort-by-latest')
-
-#Inputs to search box, you can totally omit it if you don't want to make any search
-searchBox = driver.find_element_by_xpath('//*[@id="autosuggest"]')
-searchBox.send_keys('', Keys.ENTER) #put the keywords inside the single quoatation
 
 #This loop goes through every page and grabs all the details of each listing
 #Loop will only end when there are no more pages to go through
@@ -43,26 +39,27 @@ while True:
         
         #grabing the all the tags and put them in file.txt with the same naming as the header
         tags = soup.find_all('a', class_ = 'd0KA3Wtv')
-        with open(f'path/{header}.txt', 'a') as f:
+        with open(f'C:/Users/user/Desktop/envato_crawler/{header}.txt', 'a') as f:
             #change path with the location you want
             for tag in tags:
                 f.write(tag.text+'\n')
             f.close()
-        
+        sleep(3)
         #clicking on the image to start getting all the other images
         driver.find_element_by_xpath('//*[@id="app"]/div[1]/main/div/div[1]/section[1]/div[2]/div/div[1]/div/div/div[3]/img').click()
-        WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, '/html/body/div[9]/div/div/div/div[1]/div[2]/div/button[1]')))
+        sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
         images = soup.find_all('button', class_ = 'OaSBRYFO')
         sleep(1)
         for i in range(1, len(images)+1):
             sleep(1)
-            driver.find_element_by_xpath(f'/html/body/div[9]/div/div/div/div[1]/div[2]/div/button[{i}]').click()
+            driver.find_element_by_xpath(f'/html/body/div[9]/div/div/div/div[1]/div[2]/div/button[{i}]/div').click()
             soup = BeautifulSoup(driver.page_source, 'lxml')
             image = soup.find('img', class_ = 'AjQn4Il1 undefined').get('srcset')
             sleep(2)
             response = requests.get(image)
             if response.status_code:
-                fp = open(f'path/{header}{i}.png', 'wb') #change path with the location you want
+                fp = open(f'C:/Users/user/Desktop/envato_crawler/{header}{i}.png', 'wb') #change path with the location you want
                 fp.write(response.content)
                 fp.close()
     
